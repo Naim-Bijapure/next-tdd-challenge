438fba52b0c673faf78f2c888a5d03684e2e7dd3
NaimBijapure <naim@vinnovatetechnologies.com>
2024-08-02 15:50:57 +0530

    update(test):test case 2 complete and refactor add function

--- app/components/StringCalculator/actions.ts
+++ app/components/StringCalculator/actions.ts
@@ -2,15 +2,11 @@
 
 export async function add(input: string): Promise<number> {
     // on false input value
-    if (!input) {
-        return 0;
-    }
+    if (!input) return 0;
     const numbers = input.split(",");
 
     // if there is only single number input
-    if (numbers.length === 1) {
-        return +numbers[0];
-    }
+    if (numbers.length === 1) return +numbers[0];
 
     // if multiple numbers input
     if (numbers.length > 1) {
