8282d3765b4b8643ffa132f0e476bf391eab8ffe
NaimBijapure <naim@vinnovatetechnologies.com>
2024-08-02 15:48:45 +0530

    update(test):test case 1 complete

--- app/components/StringCalculator/__tests__/add.test.ts
+++ app/components/StringCalculator/__tests__/add.test.ts
@@ -1,3 +1,14 @@
-// import { add } from "../actions";
+import { add } from "../actions";
 
-describe("String Calculator:add", () => {});
+describe("String Calculator:add", () => {
+    it("should return 0 for an empty string", async () => {
+        expect(await add("")).toBe(0);
+    });
+
+    it("should return the number itself for a single number", async () => {
+        expect(await add("1")).toBe(1);
+    });
+    it("should return the sum of two numbers", async () => {
+        expect(await add("1,5")).toBe(6);
+    });
+});
--- app/components/StringCalculator/actions.ts
+++ app/components/StringCalculator/actions.ts
@@ -1,5 +1,21 @@
 "use server";
 
 export async function add(input: string): Promise<number> {
+    // on false input value
+    if (!input) {
+        return 0;
+    }
+    const numbers = input.split(",");
+
+    // if there is only single number input
+    if (numbers.length === 1) {
+        return +numbers[0];
+    }
+
+    // if multiple numbers input
+    if (numbers.length > 1) {
+        let sum = numbers.reduce((acc, num) => acc + +num, 0);
+        return sum;
+    }
     return 0;
 }
