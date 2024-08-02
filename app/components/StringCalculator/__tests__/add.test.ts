f68ede4b3d642d85c80b2e2e9ff90760b39d3848
NaimBijapure <naim@vinnovatetechnologies.com>
2024-08-02 16:10:45 +0530

    update(add test):test case 3 complete allow new line in input number

--- app/components/StringCalculator/__tests__/add.test.ts
+++ app/components/StringCalculator/__tests__/add.test.ts
@@ -8,12 +8,18 @@ describe("String Calculator:add", () => {
     it("should return the number itself for a single number", async () => {
         expect(await add("1")).toBe(1);
     });
+
     it("should return the sum of two numbers", async () => {
         expect(await add("1,5")).toBe(6);
     });
+
     it("should return the sum of multiple numbers", async () => {
         const arr = [1, 2, 3, 5, 6, 10, 10, 30];
         const sum = arr.reduce((acc, num) => acc + num, 0);
         expect(await add(arr.join(","))).toBe(sum);
+    });
+
+    it("should handle new lines between numbers", async () => {
+        expect(await add("1\n2,3")).toBe(6);
     });
 });
--- app/components/StringCalculator/actions.ts
+++ app/components/StringCalculator/actions.ts
@@ -3,7 +3,7 @@
 export async function add(input: string): Promise<number> {
     // on false input value
     if (!input) return 0;
-    const numbers = input.split(",");
+    const numbers = extractNumbers(input.split(""));
 
     // if there is only single number input
     if (numbers.length === 1) return +numbers[0];
@@ -14,4 +14,28 @@ export async function add(input: string): Promise<number> {
         return sum;
     }
     return 0;
+}
+
+function extractNumbers(arr: string[]): number[] {
+    const result: number[] = [];
+    let currentNumber = "";
+
+    // iterate over all elements
+    for (const char of arr) {
+        if (!isNaN(Number(char)) && char !== " " && char !== "\n" && char !== ",") {
+            currentNumber += char;
+        } else {
+            if (currentNumber) {
+                result.push(Number(currentNumber));
+                currentNumber = "";
+            }
+        }
+    }
+
+    // If there's any remaining number in the string, add it to the result
+    if (currentNumber) {
+        result.push(Number(currentNumber));
+    }
+
+    return result;
 }
